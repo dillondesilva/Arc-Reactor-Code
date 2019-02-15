08aec99493e2b72cc9e4baf3a28f60268fc6d641
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-02-15 18:47:00 +1100

    Added plain red

--- plain-red-arc/plain-red-arc.ino
+++ plain-red-arc/plain-red-arc.ino
@@ -2,18 +2,19 @@
 
 #define PIN 10
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(24 + 7 + 16, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24 + 7 + 16, PIN);
 void setup() {
   // put your setup code here, to run once:
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off
+  strip.clear(); // Initialize all pixels to 'off
   for (int i=0; i < 24 + 7 + 16; i++) {
-    strip.setPixelColor(i, (51,195,247));
+    strip.setPixelColor(i, 255,0,0);
   };
   strip.show();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
 }
+  
